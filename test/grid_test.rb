91769e79e0d8a7f274c40c7faf3e15e410b8b88a
jtruong2 <jtruong2@comcast.net>
2017-05-25 08:58:32 -0600

    modified test_helper

--- test/grid_test.rb
+++ test/grid_test.rb
@@ -1,4 +1,4 @@
-require 'minitest/autorun'
+require_relative 'test_helper'
 require 'minitest/emoji'
 require './lib/grid'
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 require 'simplecov'            # These two lines must go first
 SimpleCov.start
 
-require 'minitest/autorun'     # Sets up minitest
+require 'minitest/autorun'    # Sets up minitest
                                # You could require other shared gems here, too
-Raw
+require_relative 'player'
+require_relative 'computer'
+require_relative 'test_helper'
