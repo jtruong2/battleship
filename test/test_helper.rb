2c85b1154d0e18e3ffb82ac9e07d78a515760b2b
jtruong2 <jtruong2@comcast.net>
2017-05-25 08:59:59 -0600

    modified test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,3 @@ SimpleCov.start
 
 require 'minitest/autorun'    # Sets up minitest
                                # You could require other shared gems here, too
-require_relative 'player'
-require_relative 'computer'
-require_relative 'test_helper'
